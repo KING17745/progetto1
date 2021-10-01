21bf213606a23441c18bcbe4ed5e840a94347772
KING17745 <91726084+KING17745@users.noreply.github.com>
2021-10-01 14:26:47 +0200

    group class file

---
+++ Group.java
@@ -0,0 +1,42 @@
+package trelloMain;
+
+public class Group {
+	public class Gruppo {
+
+		private int id_group;
+		private String group_name;
+		private String group_description;
+		
+		public Gruppo(int id_group,String group_name,String group_description) {
+			this.id_group=id_group;
+			this.group_name=group_name;
+			this.group_description=group_description;
+		}
+
+		public int getId_group() {
+			return id_group;
+		}
+
+		public void setId_group(int id_group) {
+			this.id_group = id_group;
+		}
+
+		public String getGroup_name() {
+			return group_name;
+		}
+
+		public void setGroup_name(String group_name) {
+			this.group_name = group_name;
+		}
+
+		public String getGroup_description() {
+			return group_description;
+		}
+
+		public void setGroup_description(String group_description) {
+			this.group_description = group_description;
+		}
+		
+	}
+
+}
